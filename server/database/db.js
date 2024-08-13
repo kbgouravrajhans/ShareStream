75d92d331ee963e56674cce0742ca9ee461018c2
Gourav Rajhans <kbgaurav004@gmail.com>
2024-08-13 16:15:07 +0530

    added .env file

--- server/database/db.js
+++ server/database/db.js
@@ -1,10 +1,10 @@
 import mongoose, { connect } from 'mongoose';
+import 'dotenv/config'
 
-const uri = 'mongodb+srv://personalgauravrajhans:kbgaurav004@file-sharing-app.uoa1c.mongodb.net/?retryWrites=true&w=majority&appName=file-sharing-app'; // Replace with your MongoDB URI
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(uri);
+        await mongoose.connect(`${process.env.DATABASE_URI}`);
         console.log("Connected to MongoDB")
     } catch (error) {
         console.error("Connection error", err);
--- server/index.js
+++ server/index.js
@@ -2,9 +2,9 @@ import express from 'express'
 import router from "./routes/router.js"
 import cors from "cors";
 import connectDB from './database/db.js';
+import 'dotenv/config'
 
 const app = express();
-const PORT = 8000;
 
 connectDB();
 
@@ -12,6 +12,6 @@ app.use(cors());
 
 app.use('/', router);
 
-app.listen(`${PORT}`, ()=> {
-    console.log(`Server running on Port - ${PORT}`)
+app.listen(`${process.env.PORT}`, ()=> {
+    console.log(`Server running on Port - ${process.env.PORT}`)
 })
